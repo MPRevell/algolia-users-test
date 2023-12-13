b7724f9ddafc04fcef08ad39a89aecb6ac286838
Matt <mprevell97@gmail.com>
2023-12-13 10:38:46 +0000

    custom algolia colors added to tailwind and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "xenon-900": "#000033",
+        "xenon-600": "#003DFF",
+        "xenon-400": "#457AFF",
+        "grey-900": "#23263B",
+        "grey-700": "#484C7A",
+        white: "#FFFFFF",
+        "grey-200": "#D6D6E7",
+      },
+
+      fontFamily: {
+        sora: ["Sora", "sans-serif"],
+        inter: ["inter", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
