4ff9a4795327130dbd47edd28cf392cea2b6b92c
Matt <mprevell97@gmail.com>
2023-12-13 12:22:21 +0000

    added usercard component to display key information for each user, this is with light background color scheme given as no dark theme introduced yet as well as brand fonts

--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,36 @@ import "./App.css";
 import { api } from "./utils";
 import type { User } from "./types";
 import { useEffect, useState } from "react";
+import UserCard from "./components/UserCard";
 
 function App() {
+  const [users, setUsers] = useState<User[]>([]);
+
   useEffect(() => {
     api<User[]>("https://jsonplaceholder.typicode.com/users").then((data) => {
-      console.log(data);
+      setUsers(data);
     });
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="bg-white py-6 sm:py-8 lg:py-12">
+        <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
+          <div className="mb-10 md:mb-16">
+            <h2 className="mb-4 text-center text-2xl font-bold text-grey-900  md:mb-6 lg:text-3x font-sora">
+              Algolia Users
+            </h2>
+            <p className="mx-auto max-w-screen-md text-center text-grey-700 md:text-lg font-inter">
+              Powering Discovery for your world, search users below...
+            </p>
+          </div>
+          <div className="grid gap-4 sm:grid-cols-2 md:gap-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-8">
+            {users.map((user) => (
+              <UserCard user={user} />
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
