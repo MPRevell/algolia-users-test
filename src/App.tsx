8535166406a005016b55e389ac5b5f0c835c715f
Matt <mprevell97@gmail.com>
2023-12-13 11:31:27 +0000

    now added the api call with typescript and looks like it is retrieving the correct data from console logging it

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { api } from "./utils";
+import type { User } from "./types";
+import { useEffect, useState } from "react";
 
 function App() {
+  useEffect(() => {
+    api<User[]>("https://jsonplaceholder.typicode.com/users").then((data) => {
+      console.log(data);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
