44e3a3e8ea7437b1074ff6a8c179cebe635212f0
Matt <mprevell97@gmail.com>
2023-12-14 08:06:04 +0000

    fixed slight bug when no users found, now background is in line with dark/light them

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -2,14 +2,13 @@ import React from "react";
 
 function NotFound() {
   return (
-    <>
-      <div className="grid place-content-center bg-white px-4 dark:bg-xenon-900">
+    <div className="min-h-screen bg-white dark:bg-xenon-900 flex justify-center items-center">
+      <div className="px-4">
         <h1 className="uppercase tracking-widest text-grey-700 dark:text-grey-200">
-          404 | No Users Found!
+          Sorry | No Users Found!
         </h1>
       </div>
-      <div className="h-full dark:bg-xenon-900"></div>
-    </>
+    </div>
   );
 }
 
